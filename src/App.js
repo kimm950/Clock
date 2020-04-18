44e13ba75c769ef167dba70ca348b2f715a6082c
Kimm <hyunsung.kim@zeals.co.jp>
2020-04-18 17:50:55 +0900

    set bacic styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
-import { getAllByTestId } from '@testing-library/react';
 
 const ClockContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;  
-  height: 800px;
-  font-size: 50px;
-  text-align: center;
-`
-const Row = styled.div`
- div {
-   margin: 20px;
- }
+  div {
+    margin: 20px;
+  }
 `
 
 class Clock extends Component {
@@ -39,14 +30,12 @@ class Clock extends Component {
     const { date } = this.state
     return (
       <ClockContainer>
-        <Row>
-          <div className="month-date">
-            {date.getFullYear()} / {date.getMonth() + 1} / {date.getDate()}
-          </div>
-          <div className="time">
-            {date.getHours()} : {date.getMinutes()} : {date.getSeconds()} : {date.getMilliseconds()}
-          </div>
-        </Row>
+        <div className="month-date">
+          {date.getMonth() + 1} . {date.getDate()} . {date.getFullYear()}
+        </div>
+        <div className="time">
+          {date.getHours()} : {date.getMinutes()} : {date.getSeconds()} : {date.getMilliseconds()}
+        </div>
       </ClockContainer>
     );
   }
