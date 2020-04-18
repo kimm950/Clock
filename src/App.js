2b410cbdd185d73ffbe350c30a1e6696c8bf3031
Kimm <hyunsung.kim@zeals.co.jp>
2020-04-18 17:24:09 +0900

    set layout

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ const ClockContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;  
-  font-size: 50px;
   height: 800px;
-  .month-date {
-    display: inline-block;
-  }
+  font-size: 50px;
+  text-align: center;
+`
+const Row = styled.div`
+ div {
+   margin: 20px;
+ }
 `
 
 class Clock extends Component {
@@ -36,12 +39,14 @@ class Clock extends Component {
     const { date } = this.state
     return (
       <ClockContainer>
-        <div className="month-date">
-          {date.getFullYear()} / {date.getMonth() + 1} / {date.getDate()}
-        </div>
-        <div className="time">
-          {date.getHours()} : {date.getMinutes()} : {date.getSeconds()} : {date.getMilliseconds()}
-        </div>
+        <Row>
+          <div className="month-date">
+            {date.getFullYear()} / {date.getMonth() + 1} / {date.getDate()}
+          </div>
+          <div className="time">
+            {date.getHours()} : {date.getMinutes()} : {date.getSeconds()} : {date.getMilliseconds()}
+          </div>
+        </Row>
       </ClockContainer>
     );
   }
