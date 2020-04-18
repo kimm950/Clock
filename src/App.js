585c8ff6e0ffb864292d62ef338eda2bc5884f67
Kimm <hyunsung.kim@zeals.co.jp>
2020-04-18 16:54:15 +0900

    render hours

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.timeID = setInterval(() => this.Change(), 1000)
+  }
+
+  componentWillMount() {
+    clearInterval()
+  }
+
   render() {
+    const { date } = this.state
     return (
-      
+      <div>
+        {date.getHours()}
+      </div>
     );
   }
 }
+
+export default Clock
